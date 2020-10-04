33fcf91091fcb7bea5b06a47762ad70515475bcd
Gora LEYE <leyegora@yahoo.fr>
2020-10-04 15:24:01 +0200

    fix service configuration

--- src/LogCorner.EduSync.Speech.ServiceBus/ServicesConfiguration.cs
+++ src/LogCorner.EduSync.Speech.ServiceBus/ServicesConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using Confluent.Kafka;
+﻿using System.Reflection;
+using Confluent.Kafka;
+using LogCorner.EduSync.Speech.ServiceBus.Mediator;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LogCorner.EduSync.Speech.ServiceBus
@@ -7,19 +10,34 @@ namespace LogCorner.EduSync.Speech.ServiceBus
     {
         public static void AddServiceBus(this IServiceCollection services, string url)
         {
-            //services.AddSingleton<IServiceBus, ServiceBus>();
 
-            //services.AddSingleton<IJsonSerializer, CustomJsonSerializer>();
+            services.AddSingleton<IServiceBus, ServiceBus>();
 
-            //services.AddSingleton<IServiceBusProvider>(x =>
-            //    {
-            //        var producerConfig = new ProducerConfig { BootstrapServers = url };
+            services.AddSingleton<IJsonSerializer, CustomJsonSerializer>();
 
-            //        var producer = new ProducerBuilder<Null, string>(producerConfig).Build();
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient<INotifierMediatorService, NotifierMediatorService>();
 
-            //        return new KafkaClient(producer, x.GetRequiredService<IJsonSerializer>());
-            //    }
-            //);
+            services.AddSingleton<IServiceBusProvider>(x =>
+                {
+                    var producerConfig = new ProducerConfig { BootstrapServers = url };
+
+                    var consumerConfig = new ConsumerConfig
+                    {
+                        GroupId = "gid-consumers",
+                        BootstrapServers = url,
+                        AutoOffsetReset = AutoOffsetReset.Earliest
+                    };
+                    var consumer = new ConsumerBuilder<Null, string>(consumerConfig).Build();
+                    var producer = new ProducerBuilder<Null, string>(producerConfig).Build();
+
+                    return new KafkaClient(producer,
+                        x.GetRequiredService<IJsonSerializer>(),
+                        consumer,
+                        x.GetRequiredService<INotifierMediatorService>()
+                    );
+                }
+            );
         }
     }
 }
