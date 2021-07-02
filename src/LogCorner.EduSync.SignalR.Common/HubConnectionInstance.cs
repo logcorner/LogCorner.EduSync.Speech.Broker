ddfcae470397581bc4058df7a7578ac9bdb63a8a
Gora LEYE <leyegora@yahoo.fr>
2021-07-02 07:30:55 +0200

    fix

--- src/LogCorner.EduSync.SignalR.Common/HubConnectionInstance.cs
+++ src/LogCorner.EduSync.SignalR.Common/HubConnectionInstance.cs
@@ -6,13 +6,14 @@ namespace LogCorner.EduSync.SignalR.Common
     public class HubConnectionInstance : IHubInstance
     {
         private readonly IIdentityProvider _identityProvider;
+
         private string Url { get; }
         public HubConnection Connection { get; private set; }
 
         public HubConnectionInstance(string url, IIdentityProvider identityProvider)
         {
-            _identityProvider = identityProvider;
             Url = url;
+            _identityProvider = identityProvider;
         }
 
         public async Task InitAsync()
@@ -24,22 +25,21 @@ namespace LogCorner.EduSync.SignalR.Common
             await Task.CompletedTask;
         }
 
-        public async Task InitConfidentialClientAsync()
-        {
-            var scopes = new[] { "https://datasynchrob2c.onmicrosoft.com/signalr/hub/.default" };
-          
-            var accessToken = await _identityProvider.AcquireTokenForClient(scopes);
-            Connection = new HubConnectionBuilder()
-                .WithUrl(Url, options =>
-                {
-                    options.AccessTokenProvider = () => Task.FromResult(accessToken);
-                })
-                .Build();
-        }
-
         public async Task StartAsync()
         {
             await Connection.StartAsync();
+        }
+
+        public async Task InitConfidentialClientAsync()
+        {
+            var scopes = new[] { "https://datasynchrob2c.onmicrosoft.com/signalr/hub/.default" };
+            var AccessToken = await _identityProvider.AcquireTokenForClient(scopes);
+            Connection = new HubConnectionBuilder()
+                .WithUrl(Url, options =>
+                {
+                    options.AccessTokenProvider = () => Task.FromResult(AccessToken);
+                })
+                .Build();
         }
     }
 }
--- src/LogCorner.EduSync.SignalR.Common/ServicesConfiguration.cs
+++ src/LogCorner.EduSync.SignalR.Common/ServicesConfiguration.cs
@@ -5,15 +5,15 @@ namespace LogCorner.EduSync.SignalR.Common
 {
     public static class ServicesConfiguration
     {
-        public static void AddSignalRServices(this IServiceCollection services, string endpoint, IConfiguration configuration)
+        public static void AddSignalRServices(this IServiceCollection services, string endpoint, IConfiguration config )
         {
             services.AddSingleton<ISignalRNotifier, SignalRNotifier>();
             services.AddSingleton<ISignalRPublisher, SignalRPublisher>();
 
             services.AddSingleton<IHubInstance, HubConnectionInstance>(ctx =>
             {
-                var hubConnectionInstance = new HubConnectionInstance(endpoint, new IdentityProvider(configuration));
-                hubConnectionInstance.InitConfidentialClientAsync().Wait();
+                var hubConnectionInstance = new HubConnectionInstance(endpoint ,new IdentityProvider(config));
+                hubConnectionInstance.InitAsync().Wait();
                 return hubConnectionInstance;
             });
         }
