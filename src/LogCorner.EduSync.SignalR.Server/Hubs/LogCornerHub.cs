1fd1b488a30c459459a100b3722582407ae77e7e
Gora LEYE <leyegora@yahoo.fr>
2021-09-01 20:58:02 +0200

    display client name

--- src/LogCorner.EduSync.SignalR.Server/Hubs/LogCornerHub.cs
+++ src/LogCorner.EduSync.SignalR.Server/Hubs/LogCornerHub.cs
@@ -1,4 +1,5 @@
 ﻿using LogCorner.EduSync.SignalR.Common;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Threading.Tasks;
@@ -9,42 +10,45 @@ namespace LogCorner.EduSync.SignalR.Server.Hubs
     // [Authorize()]
     public class LogCornerHub<T> : Hub<IHubNotifier<T>>, IHubInvoker<T> where T : class
     {
+        private string clientName { get; set; }
         public override Task OnConnectedAsync()
         {
-            Console.WriteLine($"OnConnectedAsync :: client : {Context.ConnectionId}");
+            var httpContext = Context.GetHttpContext();
+            clientName = httpContext.Request.Query["clientName"];
+            Console.WriteLine($"OnConnectedAsync :: clientId : {Context.ConnectionId}, clientName : {clientName}");
             return base.OnConnectedAsync();
         }
 
         public override Task OnDisconnectedAsync(Exception? exception)
         {
-            Console.WriteLine($"OnDisconnectedAsync :: client : {Context.ConnectionId}");
+            Console.WriteLine($"OnDisconnectedAsync :: clientId : {Context.ConnectionId}, clientName : {clientName}");
             return base.OnDisconnectedAsync(exception);
         }
 
         public async Task Publish(T payload)
         {
             await Clients.All.OnPublish(payload);
-            Console.WriteLine($"Publish :: payload : {payload} , client : {Context.ConnectionId}");
+            Console.WriteLine($"Publish :: payload : {payload} , clientId : {Context.ConnectionId}, clientName : {clientName}");
         }
 
         public async Task PublishToTopic(string topic, T payload)
         {
             await Clients.All.OnPublish(topic, payload);
-            Console.WriteLine($"PublishToTopic :: topic : {topic} , payload : {payload}, client : {Context.ConnectionId}");
+            Console.WriteLine($"PublishToTopic :: topic : {topic} , payload : {payload}, clientId : {Context.ConnectionId}, clientName : {clientName}");
         }
 
         public async Task Subscribe(string topic)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, topic);
             await Clients.Groups(topic).OnSubscribe(Context.ConnectionId, topic);
-            Console.WriteLine($"Subscribe :: topic : {topic} , client : {Context.ConnectionId}");
+            Console.WriteLine($"Subscribe :: topic : {topic} , clientId : {Context.ConnectionId}, clientName : {clientName}");
         }
 
         public async Task UnSubscribe(string topic)
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, topic);
             await Clients.Groups(topic).OnUnSubscribe(Context.ConnectionId, topic);
-            Console.WriteLine($"UnSubscribe :: topic : {topic} , client : {Context.ConnectionId}");
+            Console.WriteLine($"UnSubscribe :: topic : {topic} , clientId : {Context.ConnectionId}, clientName : {clientName}");
         }
     }
 }
