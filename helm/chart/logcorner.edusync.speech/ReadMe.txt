9ea5387c980f887a2b965690c79d456a556b8cf1
Gora LEYE <leyegora@yahoo.fr>
2021-11-03 16:41:23 +0100

    add helm chart

---
+++ helm/chart/logcorner.edusync.speech/ReadMe.txt
@@ -0,0 +1,8 @@
+helm install service-bus-infrastructure logcorner.edusync.speech
+
+helm get manifest logcorner-edusync-speech
+
+helm upgrade service-bus-infrastructure logcorner.edusync.speech
+
+
+https://helm.kubernetes.docker.com/speech-command-http-api/swagger/index.html
--- src/LogCorner.EduSync.Speech.ServiceBus/ServicesConfiguration.cs
+++ src/LogCorner.EduSync.Speech.ServiceBus/ServicesConfiguration.cs
@@ -3,13 +3,15 @@ using LogCorner.EduSync.Speech.ServiceBus.Mediator;
 using LogCorner.EduSync.Speech.SharedKernel.Serialyser;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net;
 using System.Reflection;
 
 namespace LogCorner.EduSync.Speech.ServiceBus
 {
     public static class ServicesConfiguration
     {
-        public static void AddServiceBus(this IServiceCollection services, string url)
+        public static void AddServiceBus(this IServiceCollection services, string bootstrapServer)
         {
             services.AddSingleton<IServiceBus, ServiceBus>();
 
@@ -18,17 +20,51 @@ namespace LogCorner.EduSync.Speech.ServiceBus
 
             services.AddSingleton<IServiceBusProvider>(x =>
                 {
-                    var producerConfig = new ProducerConfig { BootstrapServers = url };
+                    var producerConfig = new ProducerConfig
+                    {
+                        BootstrapServers = bootstrapServer,
+                        EnableDeliveryReports = true,
+                        ClientId = Dns.GetHostName(),
+                        Debug = "msg",
+
+                        // retry settings:
+                        // Receive acknowledgement from all sync replicas
+                        Acks = Acks.All,
+                        // Number of times to retry before giving up
+                        MessageSendMaxRetries = 3,
+                        // Duration to retry before next attempt
+                        RetryBackoffMs = 1000,
+                        // Set to true if you don't want to reorder messages on retry
+                        EnableIdempotence = true
+                    };
+                    var producer = new ProducerBuilder<Null, string>(producerConfig)
+                   .SetKeySerializer(Serializers.Null)
+                   .SetValueSerializer(Serializers.Utf8)
+                   .SetLogHandler((_, message) =>
+                                   Console.WriteLine($"Facility: {message.Facility}-{message.Level} Message: {message.Message}"))
+                   .SetErrorHandler((_, e) =>
+                                   Console.WriteLine($"Error: {e.Reason}. Is Fatal: {e.IsFatal}"))
+                   .Build();
 
                     var consumerConfig = new ConsumerConfig
                     {
-                        GroupId = "gid-consumers",
-                        BootstrapServers = url,
+                        BootstrapServers = bootstrapServer,
+                        EnableAutoCommit = false,
+                        EnableAutoOffsetStore = false,
+                        MaxPollIntervalMs = 300000,
+                        GroupId = "default",
+
+                        // Read messages from start if no commit exists.
                         AutoOffsetReset = AutoOffsetReset.Earliest
                     };
-                    var consumer = new ConsumerBuilder<Null, string>(consumerConfig).Build();
-                    var producer = new ProducerBuilder<Null, string>(producerConfig).Build();
 
+                    var consumer = new ConsumerBuilder<Null, string>(consumerConfig)
+                   .SetKeyDeserializer(Deserializers.Null)
+                   .SetValueDeserializer(Deserializers.Utf8)
+                   .SetErrorHandler((_, e) =>
+                                      Console.WriteLine($"Error: {e.Reason}"))
+                   .Build();
+                    // consumer.
                     return new KafkaClient(producer,
                         x.GetRequiredService<IJsonSerializer>(),
                         consumer,
